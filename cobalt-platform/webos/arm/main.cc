7970bebca65f874c33caa914db7a888f983e3702
Ronald Fiering <rfiering@googlemail.com>
2026-09-25 20:49:49 +0200

    Fix Cobalt log size captures

--- cobalt-platform/webos/arm/main.cc
+++ cobalt-platform/webos/arm/main.cc
@@ -36,7 +36,7 @@ extern "C" SB_EXPORT_PLATFORM int main(int argc, char** argv) {
     dup2(log_pipe[1], STDERR_FILENO);
     close(log_pipe[1]);
 
-    std::thread([read_fd = log_pipe[0], log_path]() {
+    std::thread([read_fd = log_pipe[0], log_path, kMaxLogBytes,\n                 kRetainedLogBytes]() {
       FILE* log = std::fopen(log_path, "a+b");
 
       auto trim_log = [&]() {
